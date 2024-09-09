8cc42f0f06844302a19fccf7f962416876176d91
Dulce-Maria-Tolentino-De-Anda <a.22308051281367@cbtis128.edu.mx>
2024-09-09 10:14:03 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,22 @@
+print("ejemplo listas")
+arcoiris=["verde","azul","morado"]
+
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris",longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print ("acccediendo a un elemento")
+print (arcoiris[1])
+print (f"elemento en la posicion 0 rs {arcoiris[0]}")
+print (f"el primer color del arcoiris es: ",arcoiris[0])
+print ("agregar a un elemento de la lista")
+arcoiris.append("naranja")
+print(arcoiris)
+print ("listar los elementos de la lista ciclo for")
+for tolentino in arcoiris:
+    print(tolentino)
+    
+
+
+
+
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print ("ejepmlo de tupas")
+canciones=("Te amo","el noa noa", "amor eterno")
+print (canciones)
+y = list (canciones)
+print (y)
+y[1] = "La puerta negra"
+x = tuple(y)
+print(x)
