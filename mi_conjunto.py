7aaeb2225c063017b74d6aae42766834d197d32f
Dulce-Maria-Tolentino-De-Anda <a.22308051281367@cbtis128.edu.mx>
2024-09-11 07:39:13 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print ("ejemplo conjuntos set")
+tipodeportes={"furbol","tenis","basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for Tolentino in tipodeportes:
+    print(Tolentino)
--- mi_tuplas.py
+++ mi_tuplas.py
@@ -1,8 +1,12 @@
 print ("ejepmlo de tupas")
 canciones=("Te amo","el noa noa", "amor eterno")
 print (canciones)
-y = list (canciones)
+y = list(canciones)
 print (y)
-y[1] = "La puerta negra"
+y[1] = "La puerta negra" 
 x = tuple(y)
 print(x)
+print ("listado de elementos de la tupla canciones ciclo for")
+
+for Tolentino in canciones:
+    print (Tolentino)
